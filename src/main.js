703d8c59eb0a4dcb11f112c251dcd6751cffe146
Allan96 <allan.souza.silva@usp.br>
2020-04-18 22:08:32 -0300

    Adicionando as páginas e as rotas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+import router from './router/index.js';
 
 Vue.use(VueResource);
 
 new Vue({
-    render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    render: h => h(App)
+})
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+
+import Index from '../pages/Index.vue'
+import RoomCorretor from '../pages/RoomCliente.vue'
+import RoomCliente from '../pages/RoomCliente.vue'
+
+Vue.use(VueRouter);
+const routes = [{
+        path: '*',
+        component: Index
+    },
+    {
+        path: '/room/corretor',
+        component: RoomCorretor
+    },
+    {
+        path: '/room/cliente',
+        component: RoomCliente
+    }
+]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+
+export default router
